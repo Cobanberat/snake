84329010bd7373c72bf431bb0270804e6c86c076
Cobanberat <cobanberat71@gmail.com>
2024-05-20 09:36:38 +0300

    yön tuşlarına tıklanınca hareket sağlandı

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
     var beforeClick = null;
-    (document).addEventListener("keydown", function (e) {
-        if (e.key === "ArrowUp" && e.key !== beforeClick) {
-            console.log("yukarı");
-        } else if (e.key === "ArrowDown" && e.key !== beforeClick) {
-            console.log("aşağı");
-        } else if (e.key === "ArrowLeft" && e.key !== beforeClick) {
-            console.log("sol");
-        } else if (e.key === "ArrowRight" && e.key !== beforeClick) {
-            console.log("sağ");
+    var sol = null; // Sol ok tuşu değeri için bir değişken tanımlıyoruz
+
+    document.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowLeft") { 
+            $( ".snake" ).animate({ "left": "-=50px" }, "slow" );
+        }
+        if (e.key === "ArrowUp") { 
+            $( ".snake" ).animate({ "top": "-=50px" }, "slow" );
+        }
+        if (e.key === "ArrowRight") { 
+            $( ".snake" ).animate({ "left": "+=50px" }, "slow" );
+        }
+        if (e.key === "ArrowDown") { 
+            $( ".snake" ).animate({ "top": "+=50px" }, "slow" );
         }
     });
-    
-
-})
+});
